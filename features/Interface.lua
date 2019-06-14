8c8b7fe187264d252e3ea5ed5a133ff973d9b3b1
Soleo <soleoofficial@gmail.com>
2019-06-14 19:39:12 +0200

    update

--- features/Interface.lua
+++ features/Interface.lua
@@ -99,3 +99,90 @@ NuttenhClient.main_frame.statusbar.value:SetPoint("CENTER", NuttenhClient.main_f
 NuttenhClient.main_frame.statusbar.value:SetFont("Fonts\\FRIZQT__.TTF", 13, "OUTLINE")
 NuttenhClient.main_frame.statusbar.value:SetTextColor(255, 255, 0)
 NuttenhClient.main_frame.statusbar.value:SetText("0%")
+
+-- Mission List
+NuttenhClient.main_frame.mission_list = CreateFrame("Frame", "NuttenhClient_MainFrame_MissionList", NuttenhClient.main_frame)
+NuttenhClient.main_frame.mission_list:SetWidth(250)
+NuttenhClient.main_frame.mission_list:SetHeight(250)
+NuttenhClient.main_frame.mission_list:SetPoint("TOP", 0, -70)
+NuttenhClient.main_frame.mission_list:SetBackdrop({
+	edgeFile="Interface/Tooltips/UI-Tooltip-Border", 
+	tile=false,
+	tileSize=64, 
+	edgeSize=10, 
+	insets={
+		left=4,
+		right=4,
+		top=4,
+		bottom=4
+	}
+})
+
+NuttenhClient.main_frame.mission_list.background = NuttenhClient.main_frame.mission_list:CreateTexture(nil, "BACKGROUND") 
+NuttenhClient.main_frame.mission_list.background:SetAllPoints(NuttenhClient.main_frame.mission_list) 
+NuttenhClient.main_frame.mission_list.background:SetTexture(0, 0, 0, 0.1) 
+
+-- Mission list scroll bar
+NuttenhClient.main_frame.mission_list.scrollframe = CreateFrame("ScrollFrame", nil, NuttenhClient.main_frame.mission_list) 
+NuttenhClient.main_frame.mission_list.scrollframe:SetPoint("TOPLEFT", 10, -10) 
+NuttenhClient.main_frame.mission_list.scrollframe:SetPoint("BOTTOMRIGHT", -10, 10)
+
+NuttenhClient.main_frame.mission_list.scrollframe = NuttenhClient.main_frame.mission_list.scrollframe 
+
+--scrollbar 
+NuttenhClient.main_frame.mission_list.scrollframe.scrollbar = CreateFrame("Slider", nil, NuttenhClient.main_frame.mission_list.scrollframe, "UIPanelScrollBarTemplate") 
+NuttenhClient.main_frame.mission_list.scrollframe.scrollbar:SetPoint("TOPLEFT", NuttenhClient.main_frame.mission_list, "TOPRIGHT", -20, -18) 
+NuttenhClient.main_frame.mission_list.scrollframe.scrollbar:SetPoint("BOTTOMLEFT", NuttenhClient.main_frame.mission_list, "BOTTOMRIGHT", -20, 18) 
+NuttenhClient.main_frame.mission_list.scrollframe.scrollbar:SetMinMaxValues(1, 200) 
+NuttenhClient.main_frame.mission_list.scrollframe.scrollbar.scrollStep = 200 / 100
+NuttenhClient.main_frame.mission_list.scrollframe.scrollbar:SetValueStep(NuttenhClient.main_frame.mission_list.scrollframe.scrollbar.scrollStep) 
+NuttenhClient.main_frame.mission_list.scrollframe.scrollbar:SetValue(0) 
+NuttenhClient.main_frame.mission_list.scrollframe.scrollbar:SetWidth(16) 
+NuttenhClient.main_frame.mission_list.scrollframe.scrollbar:SetFrameLevel(1)
+NuttenhClient.main_frame.mission_list.scrollframe.scrollbar:SetScript("OnValueChanged", function (self, value) 
+	self:GetParent():SetVerticalScroll(value) 
+end)
+
+NuttenhClient.main_frame.mission_list.scrollframe.scrollbar.background = NuttenhClient.main_frame.mission_list.scrollframe.scrollbar:CreateTexture(nil, "BACKGROUND") 
+NuttenhClient.main_frame.mission_list.scrollframe.scrollbar.background:SetAllPoints(scrollbar) 
+NuttenhClient.main_frame.mission_list.scrollframe.scrollbar.background:SetTexture(0, 0, 0, 0) 
+
+--content frame 
+NuttenhClient.main_frame.mission_list.content = CreateFrame("Frame", nil, NuttenhClient.main_frame.mission_list.scrollframe) 
+NuttenhClient.main_frame.mission_list.content:SetSize(250, 250) 
+
+NuttenhClient.main_frame.mission_list.scrollframe:SetScrollChild(NuttenhClient.main_frame.mission_list.content)
+
+-- Rewards
+NuttenhClient.main_frame.reward = CreateFrame("Frame", nil, NuttenhClient.main_frame)
+NuttenhClient.main_frame.reward:SetPoint("BOTTOMLEFT", 0, 60)
+NuttenhClient.main_frame.reward:SetHeight(27)
+NuttenhClient.main_frame.reward:SetWidth(27)
+
+NuttenhClient.main_frame.reward.value = NuttenhClient.main_frame.reward:CreateFontString(nil, "OVERLAY")
+NuttenhClient.main_frame.reward.value:SetPoint("CENTER", NuttenhClient.main_frame.reward, "TOP", 100, 20)
+NuttenhClient.main_frame.reward.value:SetFont("Fonts\\MORPHEUS.ttf", 18)
+NuttenhClient.main_frame.reward.value:SetTextColor(0, 0, 0, 0.8)
+NuttenhClient.main_frame.reward.value:SetText("Le vainqueur remportera :")
+
+NuttenhClient.main_frame.noReward = NuttenhClient.main_frame:CreateFontString(nil, "ARTWORK")
+NuttenhClient.main_frame.noReward:SetFont("Fonts\\ARIALN.ttf", 17)
+NuttenhClient.main_frame.noReward:SetPoint("BOTTOM", 0, 55)
+NuttenhClient.main_frame.noReward:SetText("Les officiers n'ont déterminé \n aucune récompense pour cet évènement")
+NuttenhClient.main_frame.noReward:SetTextColor(0, 0, 0, 1)
+
+if getArraySize(vGet("rewards")) == 0 or getArraySize(vGet("rewards")) == nil then
+	NuttenhClient.main_frame.noReward:Show()
+	NuttenhClient.main_frame.reward:Hide()
+else
+	NuttenhClient.main_frame.noReward:Hide()
+	NuttenhClient.main_frame.reward:Show()
+end
+
+function addMissionLine(text, lineNumber)
+	NuttenhClient.main_frame.mission_list.content[lineNumber] = NuttenhClient.main_frame.mission_list.content:CreateFontString(nil, "ARTWORK")
+	NuttenhClient.main_frame.mission_list.content[lineNumber]:SetFont("Fonts\\ARIALN.ttf", 15)
+	NuttenhClient.main_frame.mission_list.content[lineNumber]:SetPoint("TOPLEFT", 0, 0 - ((lineNumber - 1) * 35))
+	NuttenhClient.main_frame.mission_list.content[lineNumber]:SetText("- " .. text)
+	NuttenhClient.main_frame.mission_list.content[lineNumber]:SetTextColor(0, 0, 0, 1)
+end
