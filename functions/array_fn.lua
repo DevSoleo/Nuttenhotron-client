d3e7873e4d6e525c37bbf1108eb4c0534fa569fd
Soleo <soleoofficial@gmail.com>
2019-07-15 21:58:53 +0200

    update

--- functions/array_fn.lua
+++ functions/array_fn.lua
@@ -29,3 +29,15 @@ function concatArray(t1, t2)
 
     return t1
 end
+
+function shuffleTable(t)
+    local rand = math.random 
+    assert(t, "table.shuffle() expected a table, got nil")
+    local iterations = #t
+    local j
+
+    for i = iterations, 2, -1 do
+        j = rand(i)
+        t[i], t[j] = t[j], t[i]
+    end
+end
--- main.lua
+++ main.lua
@@ -34,56 +34,62 @@ NuttenhClient.stupid_race.itemList = {}
     Gray = 2
     Green = 3
 ]]
-local colors = {}
-colors[1] = "Interface\\COMMON\\Indicator-Red.png"
-colors[2] = "Interface\\COMMON\\Indicator-Yellow.png"
-colors[3] = "Interface\\COMMON\\Indicator-Gray.png"
-colors[4] = "Interface\\COMMON\\Indicator-Green.png"
+NuttenhClient.stupid_race.colors = {}
+NuttenhClient.stupid_race.colors[1] = "Interface\\COMMON\\Indicator-Red.png"
+NuttenhClient.stupid_race.colors[2] = "Interface\\COMMON\\Indicator-Yellow.png"
+NuttenhClient.stupid_race.colors[3] = "Interface\\COMMON\\Indicator-Gray.png"
+NuttenhClient.stupid_race.colors[4] = "Interface\\COMMON\\Indicator-Green.png"
 
-local selectedColor = colors[1]
-table.remove(colors, 1)
-table.sort(colors)
+local selectedColorId = 4
+local selectedColor = NuttenhClient.stupid_race.colors[selectedColorId]
 
-local function shuffleTable(t)
-    local rand = math.random 
-    assert(t, "table.shuffle() expected a table, got nil")
-    local iterations = #t
-    local j
+table.remove(NuttenhClient.stupid_race.colors, selectedColorId)
+table.sort(NuttenhClient.stupid_race.colors)
 
-    for i = iterations, 2, -1 do
-        j = rand(i)
-        t[i], t[j] = t[j], t[i]
-    end
+shuffleTable(NuttenhClient.stupid_race.colors)
 
-    print(t[1] .. " " .. t[2] .. " " .. t[3])
-end
-
-shuffleTable(colors)
-
--- Affichage des lignes/cases
+-- Affichage des colonnes
 for a = 0, 3 do
     NuttenhClient.stupid_race.itemList[a] = {}
 
+    -- Affichage des lignes/cases
     for i = 0, 31 do
+        local tooltipText = ""
         local texture = ""
 
-        if i == 0 then
-            if a == 0 then
-                texture = selectedColor
-            else
-                texture = colors[a]
-            end
-        else
-            texture = "Interface\\PaperDoll\\UI-Backpack-EmptySlot"
-        end
-
+        -- On créée l'icône (cercle de couleur, fond d'item)
         NuttenhClient.stupid_race.itemList[a][i] = {}
         NuttenhClient.stupid_race.itemList[a][i] = CreateFrame("Frame", nil, NuttenhClient.stupid_race.frame)
+
         NuttenhClient.stupid_race.itemList[a][i]:SetFrameStrata("BACKGROUND")
         NuttenhClient.stupid_race.itemList[a][i]:SetBackdropBorderColor(255, 0, 0, 1)
         NuttenhClient.stupid_race.itemList[a][i]:SetPoint("TOPLEFT", i * 25 + 30, a * -35 - 35)
         NuttenhClient.stupid_race.itemList[a][i]:SetWidth(25) -- Set these to whatever height/width is needed 
         NuttenhClient.stupid_race.itemList[a][i]:SetHeight(25) -- for your Texture
+
+        if i == 0 then
+            if a == 0 then
+                texture = selectedColor
+                tooltipText = "Joueur - " .. UnitName("player")
+                NuttenhClient.stupid_race.itemList[a][i]:SetFrameLevel(5)
+            else
+                texture = NuttenhClient.stupid_race.colors[a]
+                tooltipText = "Joueur - " .. a + 1
+                NuttenhClient.stupid_race.itemList[a][i]:SetFrameLevel(5)
+            end
+        else
+            texture = "Interface\\PaperDoll\\UI-Backpack-EmptySlot"
+        end
+
+        NuttenhClient.stupid_race.itemList[a][i]:SetScript("OnEnter", function(self)
+            GameTooltip:SetOwner(NuttenhClient.stupid_race.itemList[a][i], "ANCHOR_CURSOR")
+            GameTooltip:SetText(tooltipText)
+            GameTooltip:Show()
+        end)
+
+        NuttenhClient.stupid_race.itemList[a][i]:SetScript("OnLeave", function(self)
+            GameTooltip:Hide()
+        end)
 
         local t = NuttenhClient.stupid_race.itemList[a][i]:CreateTexture(nil,"BACKGROUND")
         t:SetTexture(texture)
@@ -91,3 +97,19 @@ for a = 0, 3 do
         NuttenhClient.stupid_race.itemList[a][i].texture = t
     end
 end
+
+NuttenhClient.stupid_race.move_button = CreateFrame("Button", "aze", NuttenhClient.stupid_race.frame, "GameMenuButtonTemplate")
+NuttenhClient.stupid_race.move_button:SetPoint("BOTTOM", 0, 50)
+NuttenhClient.stupid_race.move_button:SetFrameLevel(5)
+NuttenhClient.stupid_race.move_button:SetHeight(30)
+NuttenhClient.stupid_race.move_button:SetWidth(100)
+NuttenhClient.stupid_race.move_button:SetText("Valider")
+
+NuttenhClient.stupid_race.move_button:SetScript("OnClick", function(self)
+    local row = 15
+
+    for line=0, row do
+        NuttenhClient.stupid_race.itemList[2][0]:SetPoint("TOPLEFT", row * 25 + 30, line * -35 - 35)
+    end
+end)
+
